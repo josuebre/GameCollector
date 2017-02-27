7257fd724c5241d9b9d7e04f4ccd2fe96014b0ed
Josue Breton Mini <josuebre@gmail.com>
2017-02-27 11:48:39 -0800

    Antes de agregar funciones a la cámara, pero ya esta todo el View Controller listo para con cámara y photos…etc.

---
+++ GameCollector/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  GameCollector
+//
+//  Created by Josue Breton Mini on 2/27/17.
+//  Copyright © 2017 josuebre Mini. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var titleTexField: UITextField!
+    
+    @IBOutlet weak var gameImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func PhotosTapped(_ sender: Any) {
+    }
+
+    @IBAction func CameraTapped(_ sender: Any) {
+    }
+    @IBAction func AddTapped(_ sender: Any) {
+    }
+}
